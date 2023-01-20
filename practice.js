11ff8ec4dc716b49c9d86387a936ab69c2ce23b4
Naman <naman.khandelwal@intimetec.com>
2023-01-20 11:48:01 +0530

    create a js file and practice js code

---
+++ practice.js
@@ -0,0 +1,36 @@
+// this si practice file for practice for javascript 
+// variable type in js 
+
+
+// let comes with limited scope 
+// let variable scope is inside the block 
+// And let variable is not be redeclare 
+// let a=9;
+// let b=9;
+
+
+// // scope in the whole function 
+// var a1=0;
+// var a2=0;
+
+
+// // here const variabel is not chage if they inilished once 
+// const a3=0
+
+{
+    let a=10;
+    var b=20
+    console.log(a);
+}
+
+// console.log(a);
+console.log(b);
+
+function display() {
+    // here date is built in function that is used for take current time 
+ document.getElementById("demo").innerHTML = Date();
+ document.getElementById("demo1").innerHTML = "current time is ";
+ console.log("you clicked the button");
+}
+
+
